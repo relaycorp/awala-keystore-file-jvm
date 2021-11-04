a8ef00ac9f73fb7f144ea099d82aaa8bdef9982a
Gus Narea <gnarea@users.noreply.github.com>
2021-11-04 22:58:53 +0000

    agree to gradle scans T&Cs

--- build.gradle.kts
+++ build.gradle.kts
@@ -176,3 +176,10 @@ tasks.publish {
 configure<org.jlleitschuh.gradle.ktlint.KtlintExtension> {
     version.set("0.42.1")
 }
+
+gradleEnterprise {
+    buildScan {
+        termsOfServiceUrl = "https://gradle.com/terms-of-service"
+        termsOfServiceAgree = "yes"
+    }
+}
